729c4951b4957d2d0713f809fa76fcd284572992
Tony <tonyasdzxc@gmail.com>
2021-09-06 19:06:50 +0800

    remove unused code

--- src/three/Viewer.ts
+++ src/three/Viewer.ts
@@ -61,7 +61,6 @@ export default class Viewer
 
         this.ARAP = new ARAP(this.viewportControls.camera, this.objectMgr, this.scene, this.container);
 
-        this.testWarpDegree = 0;
         this.soruceTestPoints = [];
         this.targetTestPoints = [];
         this.testDelaunayRemesh();
@@ -72,11 +71,6 @@ export default class Viewer
     {
         //Basic gui setting
         this.datGUI.width = '450px'
-
-        const uniformWarpFolder = this.datGUI.addFolder('UniformWarp');
-        uniformWarpFolder.add(this, 'testWarpDegree', -30, 30, 1).name('warpDeg').onChange(this.warp.bind(this));
-        uniformWarpFolder.add(this, 'resetViewPort');
-        uniformWarpFolder.add(this, 'resetWarp');
 
         const ARAPFolder = this.datGUI.addFolder('ARAP');
         ARAPFolder.open();
@@ -264,56 +258,6 @@ export default class Viewer
         {
             point.visible = !point.visible
         });
-    }
-
-    warp()
-    {
-        //reset
-        this.resetWarp()
-
-        //apply rotation deg of object
-        const matrix = new THREE.Matrix4().makeRotationY(THREE.MathUtils.degToRad(this.testWarpDegree));
-        this.testMesh.applyMatrix4(matrix);
-        this.testMesh.updateMatrix();
-        this.testMesh.updateMatrixWorld();
-
-        //copy origin vertices position
-        const geo = this.testMesh.geometry as THREE.PlaneBufferGeometry;
-        const positionAttribute = geo.getAttribute('position');
-        var vertices = geo.attributes.position.array;
-
-        // apply position to world space
-        const vertexWorldPos: THREE.Vector3[] = [];
-        for (let i = 0; i < vertices.length; i = i + 3)
-        {
-            const vec = new THREE.Vector3().fromBufferAttribute(positionAttribute, i / 3);
-            this.testMesh.localToWorld(vec)
-            vertexWorldPos.push(vec);
-        }
-
-        //apply revert rotation deg of object
-        const revertMatrix = new THREE.Matrix4().makeRotationY(-THREE.MathUtils.degToRad(this.testWarpDegree));
-        this.testMesh.applyMatrix4(revertMatrix);
-        this.testMesh.updateMatrix();
-        this.testMesh.updateMatrixWorld();
-
-        // apply position to local space
-        for (let i = 0; i < vertexWorldPos.length; i++)
-        {
-            this.testMesh.worldToLocal(vertexWorldPos[i]);
-        }
-
-        for (let i = 0; i < vertices.length; i = i + 3)
-        {
-            //a vertex' position is (vertices[i],vertices[i+1],vertices[i+2])
-            if (vertices[i] == -50 || vertices[i] == 50)
-                continue;
-            if (vertices[i + 1] == -50 || vertices[i + 1] == 50)
-                continue;
-            const vertexIndex = i / 3;
-            geo.attributes.position.setXY(vertexIndex, vertexWorldPos[vertexIndex].x, vertexWorldPos[vertexIndex].y);
-        }
-        geo.attributes.position.needsUpdate = true;
     }
 
     resetWarp()
